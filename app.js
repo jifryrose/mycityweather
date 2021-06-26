923f764f8bf19453a5f439ecb7f373da63d4ab92
jifryrose <54110752+jifryrose@users.noreply.github.com>
2021-06-26 13:32:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=7b801ba189998ffced3d89dfbc767328')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Description - "+descValue;
+  temp.innerHTML = "Temperature - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Sorry; We couldn't find the city!"));
+})
+
